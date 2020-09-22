4a9160d4e258700e3cb6a6712660e788539f78b8
CryoEagle <43248056+CryoEagle@users.noreply.github.com>
2020-09-22 19:03:43 +0200

    character select nástřel

---
+++ Educhem2d/Assets/scripts/game/characterSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class characterSelect : MonoBehaviour
+{
+    List<Button> charactersButtons;
+    void Start()
+    {
+        GameObject[] characterButtons = GameObject.FindGameObjectsWithTag("characterSelectBtn");
+        for (int i = 0; i < characterButtons.Length; i++)
+        {
+            charactersButtons.Add(characterButtons[i].GetComponent<Button>());
+        }
+    }
+
+    public void characterSelected()
+    {
+
+    }
+}
---
+++ Educhem2d/Assets/scripts/game/playerBasicScript/playerMovementV2.cs
@@ -0,0 +1,56 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovementV2 : MonoBehaviourPun
+{
+    public float moveSpeed = 5f;
+
+    Rigidbody2D rb;
+    PhotonView pv;
+
+    Vector2 movement;
+
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        pv = GetComponent<PhotonView>();
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+
+    private void Update()
+    {
+        if (pv.IsMine)
+        {
+            //Get the Screen positions of the object
+            Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+            //Get the Screen position of the mouse
+            Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+            //Get the angle between the points
+            float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+            //Ta Daaa
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (pv.IsMine)
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
